9259c78fee25a9e4b651b69e5b45e6eff2edfa60
rezaa_mzk <reza.mzk@gmail.com>
2023-10-01 20:29:39 +0330

    Logistic Regression is ready to be pushed.

--- 03 - Logistic Regression/logistic_regression_implementation.py
+++ 03 - Logistic Regression/logistic_regression_implementation.py
@@ -1,24 +1,53 @@
-# Logistic Regression Implementation From Scratch
+#!/usr/bin/env python
+# coding: utf-8
 
-## Importing the Libraries
+# # Logistic Regression Implementation From Scratch
+
+# ## Importing Necessary Libraries
+
+# In[1]:
+
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-## Importing the Dataset
+# ## Importing the Dataset
+
+# In[2]:
+
 
 from sklearn import datasets
 bc = datasets.load_breast_cancer()
 X, y = bc.data, bc.target
 
 
-## Splitting the Dataset into the Training set and Test set
+# ## Splitting the Dataset into the Training, Cross Validation and Test sets
+
+# In[3]:
+
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42)
+X_train, X_, y_train, y_ = train_test_split(X, y, test_size = 0.40, random_state = 42)
+X_CV, X_test, y_CV, y_test = train_test_split(X_, y_, test_size = 0.50, random_state = 42)
 
 
-## Defining the Logistic Regression Class
+# ## Feature Scaling
+
+# In[4]:
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_CV = sc.transform(X_CV)
+X_test = sc.transform(X_test)
+
+
+# ## Defining the Custom Logistic Regression Class
+
+# In[5]:
+
 
 class CustomLogisticRegression():
 
@@ -55,15 +84,38 @@ class CustomLogisticRegression():
         
 
 
-## Fit model and predict test results
+# ## Creating Instance and Fitting the model
+
+# In[6]:
+
 
 clf = CustomLogisticRegression()
 clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
 
 
-## Check accuracy score
+# ## Making Predictions
 
-from sklearn.metrics import accuracy_score
-accuracy_score(y_test, y_pred)
+# In[7]:
+
+
+y_train_pred = clf.predict(X_train)
+y_CV_pred = clf.predict(X_CV)
+y_test_pred = clf.predict(X_test)
+
+
+# ## Evaluate the model and displaying the confusion matrix
+
+# In[8]:
+
+
+from sklearn import metrics
+print("Training set accuracy         %", metrics.accuracy_score(y_train, y_train_pred) * 100)
+print("Cross validation set accuracy %", metrics.accuracy_score(y_CV, y_CV_pred) * 100)
+print("Test set accuracy             %", metrics.accuracy_score(y_test, y_test_pred) * 100)
+
+cm = metrics.confusion_matrix(y_test, y_test_pred)
+disp = metrics.ConfusionMatrixDisplay(cm)
+disp.plot()
+
+plt.show()
 
